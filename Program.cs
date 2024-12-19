60d8dd55e1657888b0a7fdca1651761cf47dbca7
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-12-19 11:36:05 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Assignment_7._2._1
+{
+    internal class Program
+    {
+        static void ShellSort(int[] arr)
+        {
+            int n = arr.Length;
+            int gap = n / 2;
+
+            while (gap > 0)
+            {
+                for (int i = gap; i < n; i++)
+                {
+                    int temp = arr[i];
+                    int j = i;
+
+                    while (j >= gap && arr[j - gap] > temp)
+                    {
+                        arr[j] = arr[j - gap];
+                        j -= gap;
+                    }
+
+                    arr[j] = temp;
+                }
+                gap /= 2; 
+            }
+        }
+    }
+}
